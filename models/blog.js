c65fc2011b7c8623b036f85a57b10642aadd6127
Funya1337 <funya7654@gmail.com>
2021-06-11 18:53:46 +0300

    feat(server): add database and user saving

---
+++ models/blog.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    },
+    signedIn: {
+        type: Boolean
+    },
+    userId: {
+        type: Number,
+        unique: true
+    }
+}, { timestamps: true });
+
+const Blog = mongoose.model('Blogs', blogSchema);
+module.exports = Blog;
--- server.js
+++ server.js
@@ -1,21 +1,84 @@
 const express = require("express");
-// import Strategy as VKStrategy from "passport-vkontakte";
-// passport.use(new VKStrategy(options, verify));
-const app = express()
+const mongoose = require("mongoose");
+const Blog = require('./models/blog');
+const app = express();
+app.use(express.json({ limit: '1mb' }))
 
 const PORT = process.env.PORT || 8080;
+
+// connect to MongoDB
+const dbURI = 'mongodb+srv://retro:Britva01@nodegambit.hjc0u.mongodb.net/example-db?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => listenMethod(), console.log("connected to db"))
+	.catch((err) => console.log(err));
 
 // Middleware
 app.set('view engine', 'ejs');
 
+app.post('/post-example', (req, res) => {
+	console.log(req.body);
+	const data = req.body;
+	res.json({
+		status: 'success',
+		firstName: data.first_name,
+		lastName: data.last_name,
+		userId: data.userId
+	});
+	const blog = new Blog({
+		first_name: data.first_name,
+		last_name: data.last_name,
+		balance: 1000,
+		signedIn: true,
+		userId: data.userId
+	});
+
+	blog.save()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => console.log(err));
+});
+
+app.get('/add-blog', (req, res) => {
+	const blog = new Blog({
+		first_name: 'borov',
+		last_name: '123',
+		balance: 1000
+	});
+
+	blog.save()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => console.log(err));
+});
+
+app.get('/all-blogs', (req, res) => {
+	Blog.find()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => console.log(err));
+});
+
+app.get('/single-blog', (req, res) => {
+	Blog.findById('60c325eb6e54f23200adb173')
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => console.log(err));
+});
+
 app.get('/', (req, res) => {
 	res.render('index');
-})
+});
 
 app.get('/login', (req, res) => {
-	res.render('login')
-})
+	res.render('login');
+});
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-})
+function listenMethod() {
+	app.listen(PORT, () => {
+		console.log(`Server is running on port ${PORT}`);
+	});
+}

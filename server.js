4ff07970f935ecb1fa4c3fd2763d618b803da451
Funya1337 <funya7654@gmail.com>
2021-06-12 22:39:23 +0300

    feat(index.ejs): made some design

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Blog = require('./models/blog');
+const path = require('path');
 const app = express();
 app.use(express.json({ limit: '1mb' }))
 
@@ -13,6 +14,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.catch((err) => console.log(err));
 
 // Middleware
+app.use(express.static(__dirname + '/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.post('/post-example', (req, res) => {
